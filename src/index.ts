79b877ba78eeb2889c7b141d39b6f614174b1411
Saradhi <saradhi0001069@gmail.com>
2023-02-17 22:43:41 +0530

    init base structure

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("hello");
+})
+
+app.listen(8080,()=>{
+    console.log(`server started on 8080`);
+})
+
