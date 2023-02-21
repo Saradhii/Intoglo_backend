efee8dcdeab6e3fb70579c1991a531d0f9d22d2f
Saradhi <saradhi0001069@gmail.com>
2023-02-21 12:19:20 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import connection from "./server/db.js"
 import express from "express";
 import dotenv from "dotenv";
 import HSCodeRoute from "./routes/HSCode.route.js";
+import cors from "cors";
+
 
 dotenv.config();
 const app = express();
-
+app.use(cors({ origin: ["http://localhost:3000","https://www.metacargo.io"] }));
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 app.use("/backend",HSCodeRoute);
